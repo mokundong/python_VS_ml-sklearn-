221504b1bb155edbf10098b51ece9a11d04fd7b3
mokundong <mokundong@sina.com>
2017-07-01 16:50:31 +0800

    MKD

--- mkd/ch04_KNN/KNeighborsRegressor.py
+++ mkd/ch04_KNN/KNeighborsRegressor.py
@@ -15,14 +15,14 @@ def create_regression_data(n):
     y[::5] = 1 * (0.5 - np.random.rand(int(n/5)))
     return train_test_split(X,y,test_size=0.25,random_state=0)
 
-def test_KNeighborClassifier(*data):
+def test_KNeighborRegressor(*data):
     X_train,X_test,y_train,y_test=data
-    clf=neighbors.KNeighborsClassifier()
+    clf=neighbors.KNeighborsRegressor()
     clf.fit(X_train,y_train)
     print("Training Score:%f"%clf.score(X_train,y_train))
     print("Testing Score:%f"%clf.score(X_test,y_test))
 
-def test_KNeighborClassifier_k_w(*data):
+def test_KNeighborRegressor_k_w(*data):
     X_train,X_test,y_train,y_test=data
     ks = np.linspace(1,y_train.size,num=100,endpoint=False,dtype='int')
     weights = ['uniform','distance']
@@ -33,7 +33,7 @@ def test_KNeighborClassifier_k_w(*data):
         training_score = []
         testing_score = []
         for k in ks:
-            clf = neighbors.KNeighborsClassifier(weights=weight,n_neighbors=k)
+            clf = neighbors.KNeighborsRegressor(weights=weight,n_neighbors=k)
             clf.fit(X_train,y_train)
             testing_score.append(clf.score(X_test,y_test))
             training_score.append(clf.score(X_train,y_train))
@@ -46,7 +46,7 @@ def test_KNeighborClassifier_k_w(*data):
     ax.set_title("KNeighborClassifier")
     plt.show()
 
-def test_KNeighborClassifier_k_p(*data):
+def test_KNeighborRegressor_k_p(*data):
     X_train, X_test, y_train, y_test = data
     ks = np.linspace(1, y_train.size, num=100, endpoint=False, dtype='int')
     Ps = [1,2,10]
@@ -57,7 +57,7 @@ def test_KNeighborClassifier_k_p(*data):
         training_score = []
         testing_score = []
         for k in ks:
-            clf = neighbors.KNeighborsClassifier(p=P,n_neighbors=k)
+            clf = neighbors.KNeighborsRegressor(p=P,n_neighbors=k)
             clf.fit(X_train,y_train)
             testing_score.append(clf.score(X_test,y_test))
             training_score.append(clf.score(X_train,y_train))
@@ -72,6 +72,6 @@ def test_KNeighborClassifier_k_p(*data):
 
 if __name__ == "__main__":
     X_train,y_train,X_test,y_test = load_classification_data()
-    #test_KNeighborClassifier(X_train,y_train,X_test,y_test)
-    #test_KNeighborClassifier_k_w(X_train,y_train,X_test,y_test)
-    test_KNeighborClassifier_k_p(X_train,y_train,X_test,y_test)
+    # test_KNeighborRegressor(X_train,y_train,X_test,y_test)
+    # test_KNeighborRegressor_k_w(X_train,y_train,X_test,y_test)
+    test_KNeighborRegressor_k_p(X_train,y_train,X_test,y_test)
